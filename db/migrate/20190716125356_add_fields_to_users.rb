b12a5678f4c9c5ce9b5cdc96d46ae507b74f8627
hamzashariq7 <hamza.shariq@7vals.com>
2019-07-17 15:57:58 +0500

    Bug and format fixes

--- db/migrate/20190716125356_add_fields_to_users.rb
+++ db/migrate/20190716125356_add_fields_to_users.rb
@@ -1,8 +1,8 @@
 class AddFieldsToUsers < ActiveRecord::Migration
   def change
     add_reference :users, :company, index: true
-    add_column :users, :f_name, :string
-    add_column :users, :l_name, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
     add_column :users, :role, :string
   end
 end
