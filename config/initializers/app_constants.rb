630548c44e1614104132140a0c293136cd82e705
AyishmAziz <ayishm.aziz@7vals.com>
2019-08-06 16:44:50 +0500

    Survey categories fixed

--- config/initializers/app_constants.rb
+++ config/initializers/app_constants.rb
@@ -1,3 +1,6 @@
 PER_PAGE_SURVEYS = 6
 QUESTION_OPTIONS = ['Checkbox', 'Radio Buttons', 'Comment Box', 'True / False']
 SURVEY_TYPE = ['Private', 'Public']
+CATEGORIES = ['Community', 'Customer Feedback', 'Customer Satisfaction', 'Demographics', 
+'Education', 'Events', 'Healthcare', 'Human Resources', 'Just for Fun',
+'Political', 'Quiz', 'Other']
