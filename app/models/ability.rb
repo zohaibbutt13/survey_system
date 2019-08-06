33e4c15166609bb13f5df544e0adf88a1d6101e8
aliahmad7 <ali.ahmad@7vals.com>
2019-08-07 00:03:04 +0500

    Updated

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,7 @@ class Ability
       can [:edit, :update], UserSetting, user_id: user.id
       can [:edit, :update], CompanySetting, company_id: user.company_id
       can [:read, :create, :update, :destroy, :edit, :new, :add_question, :add_option, :delete_question, :delete_option], Survey, company_id: user.company_id
-      cannot [:destroy, :edit], user
+      cannot [:destroy], user
     elsif user.supervisor?
       can :read, User, company_id: user.company_id, role: User::ROLE[:member]
       can [:edit, :update], UserSetting, user_id: user.id if user.company.company_setting.supervisors_settings_permission?
