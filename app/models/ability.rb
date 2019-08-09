c95ac36686492ac7c84927538af22024696af224
aliahmad7 <ali.ahmad@7vals.com>
2019-08-09 17:06:13 +0500

    fixed

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -15,7 +15,7 @@ class Ability
       can [:edit, :update], UserSetting, user_id: user.id if user.company.company_setting.supervisors_settings_permission?
       can :read, Group, company_id: user.company_id
       can :calculate_surveys, User, company_id: user.company_id
-      can [:read, :create, :update, :destroy, :edit, :new, :add_question, :add_option, :delete_question, :delete_option, :survey_charts], Survey, company_id: user.company_id if user.company.company_setting.supervisors_survey_permission?
+      can [:read, :create, :update, :destroy, :edit, :new, :add_question, :add_option, :delete_question, :delete_option, :survey_charts], Survey, company_id: user.company_id, user_id: user.id if user.company.company_setting.supervisors_survey_permission?
       can [:read, :create], UserResponse
     elsif user.member? 
       can [:edit, :update], UserSetting, user_id: user.id if user.company.company_setting.members_settings_permission?    
